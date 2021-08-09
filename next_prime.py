de6e67904d6a323e90bad6f9aafabd2f388ecbe0
Muhammad Shahjahan <34799523+sharifioni@users.noreply.github.com>
2021-08-09 13:03:17 +0500

    Update next_prime.py

--- next_prime.py
+++ next_prime.py
@@ -5,9 +5,6 @@ Created on Mon Aug  9 11:51:22 2021
 @author: shahjahan
 """
 
-
-
-
 def is_prime(x):
     for  i in range(2,x):
         if(x%i==0):
@@ -34,8 +31,6 @@ def next_prime(x):
     if(flag==1):
         return (x)
     
-         
-        
 a=int(input("Enter any prime number \t"))
 b=is_prime(a)
 while(not b):
