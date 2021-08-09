d4d84a899b5f62c867b7b98ad3debe9f4e272cc9
Muhammad Shahjahan <34799523+sharifioni@users.noreply.github.com>
2021-08-09 12:50:08 +0500

    Create next_prime.py

---
+++ next_prime.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  9 11:51:22 2021
+
+@author: shahjahan
+"""
+
+
+
+
+def is_prime(x):
+    for  i in range(2,x):
+        if(x%i==0):
+            flag=0
+            break
+        else:
+            flag=1
+    if(flag==1):
+        return (next_prime(x))
+    else:  
+        return False
+        
+
+def next_prime(x):
+    flag=0
+    while (flag==0):
+        x=x+1
+        for i in range(2,x):
+            if(x%i==0):
+                flag=0
+                break
+            else:
+                flag=1
+    if(flag==1):
+        return (x)
+    
+         
+        
+a=int(input("Enter any prime number \t"))
+b=is_prime(a)
+while(not b):
+    print("Sorry, the number you entered is not prime")
+    a=int(input("Enter any prime number again \t"))
+    b=is_prime(a)
+    
+print("The next prime number is ",b)
